97f6c2610a4261b8dd888c194c1372abdb1b039f
chrislloyd876 <chrislloydlawrence@gmail.com>
2023-09-12 21:39:00 -0500

    Add UI to game
    Created a display to track the player and computer scores and the
    result of the round. When one player gets to five wins an alert is
    displayed to show the overall winner. Also removed all console messages.

--- javaScript.js
+++ javaScript.js
@@ -1,17 +1,45 @@
-const paper = document.querySelector('#paper');
-paper.addEventListener('click', function(e){
-    e.target = playRound("paper",computerSelection);
-});
+const container = document.querySelector('#container');
 
-const rock = document.querySelector('#rock');
-rock.addEventListener('click', function(e){
-    e.target = playRound("rock",computerSelection);
-});
 
-const scissors = document.querySelector('#scissors');
-scissors.addEventListener('click', function(e){
-    e.target = playRound("scissors",computerSelection);
-});
+const playScore = document.createElement('p');
+playScore.classList.add('playScore');
+
+const compScore = document.createElement('p');
+compScore.classList.add('compScore');
 
 const score = document.createElement('div');
 score.classList.add('score');
+
+const buttons = document.querySelectorAll('button');
+
+playScore.textContent = `Player: ${playerScore}`;
+container.appendChild(playScore);
+compScore.textContent = `Computer: ${computerScore}`;
+container.appendChild(compScore);
+
+buttons.forEach((button) => {
+    
+    button.addEventListener('click', function(e) {
+
+        result = playRound(button.id, computerSelection);
+        e.target = getScore(result);
+
+
+        playScore.textContent = `Player: ${playerScore}`;
+        compScore.textContent = `Computer: ${computerScore}`;
+        score.textContent = result;
+
+        container.appendChild(playScore);
+        container.appendChild(compScore);
+        container.appendChild(score);
+
+         // end of game results 
+        if(playerScore == 5){
+            alert("Congratulations! You won!");
+            }
+        else if(computerScore == 5){
+            alert("You lose! :( Better luck next time.");
+        }
+    });
+        
+  });
